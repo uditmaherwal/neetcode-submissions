8856efe7cca99bbcaacc18c9174d26fc365e6e09
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-09-06 18:31:12 +0530

    Update: three-integer-sum - submission-0

--- Data Structures & Algorithms/three-integer-sum/submission-0.java
+++ Data Structures & Algorithms/three-integer-sum/submission-0.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-
+        List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] > 0) break;
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-
-            int l = i+1, r = nums.length-1;
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int l = i+1;
+            int r = nums.length-1;
             while(l < r){
                 int sum = nums[i] + nums[l] + nums[r];
-                if(sum > 0){
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
                     r--;
+                    while (l < r && nums[l] == nums[l - 1]) l++;
+                    while (l < r && nums[r] == nums[r + 1]) r--;
                 }else if(sum < 0){
                     l++;
                 }else{
-                    list.add(Arrays.asList(nums[i] , nums[l] , nums[r]));
-                    l++;
                     r--;
-                    while(l < r && nums[l] == nums[l-1]) l++;
                 }
             }
         }
-        return list;
+        return result;
     }
 }
