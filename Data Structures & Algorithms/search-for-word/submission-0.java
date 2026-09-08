41e064dc13d9460b11e415c7494eec72bb9b5ba6
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-09-08 17:14:23 +0530

    Update: search-for-word - submission-0

--- Data Structures & Algorithms/search-for-word/submission-0.java
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -1,33 +1,38 @@
 class Solution {
-    
-    private int ROWS;
-    private int COLS;
 
-    public boolean exist(char[][] board, String word) {
-        ROWS = board.length;
-        COLS = board[0].length;
+    private int rown, coln;
 
-        for(int r = 0; r < ROWS; r++){
-            for(int c = 0; c < COLS; c++){
-                if(backtrack(board, word, r, c, 0)) return true;
-            }
+    private boolean dfs(char[][] board, String word, int r, int c, int i){
+        if(i == word.length()){
+            return true;
         }
-        return false;
-    }
 
-    private boolean backtrack(char[][] board, String word, int r, int c, int i){
-        if(i == word.length()) return true;
-
-        if(r < 0 || c < 0 || r >= ROWS || c >= COLS || board[r][c] != word.charAt(i) || board[r][c] == '#') return false;
+        if(r < 0 || c < 0 || r >= rown || c >= coln || board[r][c] != word.charAt(i) 
+        || board[r][c] == '#') return false;
 
         board[r][c] = '#';
 
-        boolean res = backtrack(board, word, r+1, c, i+1) ||
-                        backtrack(board, word, r-1, c, i+1) ||
-                        backtrack(board, word, r, c+1, i+1) || 
-                        backtrack(board, word, r, c-1, i+1);
-
+        boolean res = dfs(board, word, r + 1, c, i + 1) ||
+                dfs(board, word, r - 1, c, i + 1) ||
+                dfs(board, word, r, c + 1, i + 1) ||
+                dfs(board, word, r, c - 1, i + 1);
+                
         board[r][c] = word.charAt(i);
         return res;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        rown = board.length;
+        coln = board[0].length;
+
+        for(int r = 0; r < rown; r++){
+            for(int c = 0; c < coln; c++){
+                if(dfs(board, word, r, c, 0)){
+                    return true;
+                }
+            }
+        }
+    
+        return false;
     }
 }
