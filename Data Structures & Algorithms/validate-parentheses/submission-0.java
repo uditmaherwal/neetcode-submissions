afbcae1e2798455467c711c3fe01ff65aed223d2
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-09-25 21:42:02 +0530

    Update: validate-parentheses - submission-0

--- Data Structures & Algorithms/validate-parentheses/submission-0.java
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -1,22 +1,10 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Map<Character, Character> closeToOpen = new HashMap<>();
-        closeToOpen.put(')', '(');
-        closeToOpen.put(']', '[');
-        closeToOpen.put('}', '{');
-
-                for (char c : s.toCharArray()) {
-            if (closeToOpen.containsKey(c)) {
-                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
-            } else {
-                stack.push(c);
-            }
+        while (s.contains("()") || s.contains("{}") || s.contains("[]")) {
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
         }
-        return stack.isEmpty();
+        return s.isEmpty();
     }
 }
